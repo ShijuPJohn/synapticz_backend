5ab304e2bf05e39b79484db79c0ecfeb52e31db0
ShijuPJohn <shijupjohn4u@gmail.com>
2025-04-25 00:39:10 +0530

    core change in authentication: change cors to accept requests from any origin

--- main.go
+++ main.go
@@ -25,10 +25,12 @@ func main() {
 	log.Println("Tables Created")
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://synapticz.com, http://localhost:3000",
+		AllowOriginsFunc: func(origin string) bool {
+			return true // allow all origins dynamically
+		},
 		AllowCredentials: true,
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		ExposeHeaders:    "Set-Cookie",
 	}))
 	app.Use(logger.New())
